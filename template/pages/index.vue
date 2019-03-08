33f892929a659d094fa9c51097b2780c754c4405
liangyongsen <18922431411@189.cn>
2019-03-08 19:11:37 +0800

    添加样式

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,37 +1,106 @@
 <template>
-  <div id="app">
+  <div class="app">
     <!--上半部-->
     <div class="search-bar-wrap" style="height:500px;background-color: #ccc">
+      <div>
+        <h1>code snippet</h1>
+      </div>
+      <div style="text-align: center;">
+        <el-row class="demo-autocomplete">
+         <el-col :span="12">
+    <el-autocomplete
+      class="inline-input"
+      v-model="state2"
+      :fetch-suggestions="querySearch"
+      placeholder="输入关键字|input keywords"
+      :trigger-on-focus="false"
+      @select="handleSelect"
+      style="width:700px;height:40px"
+    ></el-autocomplete>
+  </el-col>
+        </el-row>
 
+      </div>
     </div>
     <div class="code-list">
       <!--搜索页码框   --> 
       <div class="pagination-wrap">
-        <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+        <el-pagination layout="prev, pager, next" :total="1000"></el-pagination>
       </div>
       <!--button-->
+      <div>
         <el-button round>圆角按钮</el-button>
         <el-button type="primary" round>主要按钮</el-button>
         <el-button type="success" round>成功按钮</el-button>
         <el-button type="info" round>信息按钮</el-button>
         <el-button type="warning" round>警告按钮</el-button>
         <el-button type="danger" round>危险按钮</el-button>
-       
-
+       </div>
+   
+   
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+   data() {
+      return {
+        restaurants: [],
+        state2: ''
+      };
+    },
+    methods: {
+      querySearch(queryString, cb) {
+        var restaurants = this.restaurants;
+        var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+        // 调用 callback 返回建议列表的数据
+        cb(results);
+      },
+      createFilter(queryString) {
+        return (restaurant) => {
+          return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
+      loadAll() {
+        return [
+          { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+          { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
+          { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
+          { "value": "泷千家(天山西路店)", "address": "天山西路438号" },
+          { "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
+          { "value": "贡茶", "address": "上海市长宁区金钟路633号" },
+          { "value": "豪大大香鸡排超级奶爸", "address": "上海市嘉定区曹安公路曹安路1685号" },
+          { "value": "茶芝兰（奶茶，手抓饼）", "address": "上海市普陀区同普路1435号" },
+          { "value": "十二泷町", "address": "上海市北翟路1444弄81号B幢-107" },
+          { "value": "星移浓缩咖啡", "address": "上海市嘉定区新郁路817号" },
+          { "value": "阿姨奶茶/豪大大", "address": "嘉定区曹安路1611号" },
+          { "value": "新麦甜四季甜品炸鸡", "address": "嘉定区曹安公路2383弄55号" },
+          { "value": "Monica摩托主题咖啡店", "address": "嘉定区江桥镇曹安公路2409号1F，2383弄62号1F" },
+          { "value": "浮生若茶（凌空soho店）", "address": "上海长宁区金钟路968号9号楼地下一层" },
+          { "value": "NONO JUICE  鲜榨果汁", "address": "上海市长宁区天山西路119号" },
+          { "value": "CoCo都可(北新泾店）", "address": "上海市长宁区仙霞西路" },
+          { "value": "快乐柠檬（神州智慧店）", "address": "上海市长宁区天山西路567号1层R117号店铺" },
+          { "value": "Merci Paul cafe", "address": "上海市普陀区光复西路丹巴路28弄6号楼819" },
+        ];
+      },
+      handleSelect(item) {
+        console.log(item);
+      }
+    },
+    mounted() {
+      this.restaurants = this.loadAll();
+    }
 };
 </script>
 <style>
 .app{
-text-align: center;
+  text-align: center;
 }
 .code-list{
-  text-align: center;width: 960px;
+  
+}
+.pagination-wrap{
+  margin-top: 4%
 }
 </style>
